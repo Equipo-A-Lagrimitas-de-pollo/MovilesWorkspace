94ba011a92b37d05c74afc9de13ba5a87655450c
juanfranrb22 <120564690+juanfranrb22@users.noreply.github.com>
2024-01-23 09:23:23 +0100

    Update Procesador.java

--- MovilesProject/src/main/java/com/moviles/model/entity/Procesador.java
+++ MovilesProject/src/main/java/com/moviles/model/entity/Procesador.java
@@ -1,5 +1,13 @@
 package com.moviles.model.entity;
 
+import jakarta.persistence.Id;
+
 public class Procesador {
 
+	@Id
+	private Long id;
+	private String nombre;
+	private int Numeronucleos;
+	private double gigaHerziosMaximos;
+	
 }
