b246d04b14199fcbb7cbc241d5f557292a83491e
jesuscieshna <jchaveroc01@iesarroyoharnina.es>
2024-01-23 08:33:13 +0100

    Initial commit

---
+++ MovilesProject/src/main/java/com/moviles/MovilesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.moviles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovilesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovilesProjectApplication.class, args);
+	}
+
+}
---
+++ MovilesProject/src/test/java/com/moviles/MovilesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovilesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
