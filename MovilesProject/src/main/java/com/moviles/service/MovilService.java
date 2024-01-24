709691df25552b902ba3698a94209ff6368415ca
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-01-24 13:36:37 +0100

    Retoque en los controllers

--- MovilesProject/src/main/java/com/moviles/service/MovilService.java
+++ MovilesProject/src/main/java/com/moviles/service/MovilService.java
@@ -6,10 +6,16 @@ import com.moviles.model.entity.Movil;
 
 public interface MovilService {
 	public List<Movil> filterByMarca(String nombre);
+
 	public List<Movil> filterByPrecioMin(float precioMin);
+
 	public List<Movil> filterByPrecioMax(float precioMax);
+
 	public List<Movil> filterByRamMin(int ramMin);
+
 	public List<Movil> filterByRamMax(int ramMax);
+
 	public List<Movil> filterByNFC(boolean nfc);
+
 	public List<Movil> filterByTec(String tipo);
 }
