5ef9808e14f136867bf288913eba892a18a6f9e9
jesuscieshna <jchaveroc01@iesarroyoharnina.es>
2024-01-27 12:29:10 +0100

    cerrando issue#15: add @Repsoitory

--- MovilesProject/src/main/java/com/moviles/repositories/MovilRepository.java
+++ MovilesProject/src/main/java/com/moviles/repositories/MovilRepository.java
@@ -1,9 +1,12 @@
 package com.moviles.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.moviles.model.entity.Movil;
 import com.moviles.model.entity.MovilKey;
 
+@Repository
 public interface MovilRepository extends JpaRepository<Movil, MovilKey>{
 	
 }
