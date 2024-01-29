fd3b67b5993093bd1ba7f0b91f0ec30b60b265d5
RadCres <raulduranc@hotmail.com>
2024-01-29 10:16:37 +0100

    Cerrando Issue - Entidades actualizadas

--- MovilesProject/src/main/java/com/moviles/model/entity/Modelo.java
+++ MovilesProject/src/main/java/com/moviles/model/entity/Modelo.java
@@ -5,10 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 
 public class Modelo {
 	@Id
