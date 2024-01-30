3c3ce0434ec18c0911613fd7d0a399f35ba3f9e8
RadCres <raulduranc@hotmail.com>
2024-01-30 20:33:38 +0100

    Finish Filter Test
    Todos los test de los filtros terminados.

--- MovilesProject/src/test/java/com/moviles/filter/FilterByTecTest.java
+++ MovilesProject/src/test/java/com/moviles/filter/FilterByTecTest.java
@@ -2,13 +2,27 @@ package com.moviles.filter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.List;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.moviles.OMs.MovilOM;
+import com.moviles.model.entity.Movil;
+import com.moviles.util.Intervalo;
+
+@SpringBootTest
 class FilterByTecTest {
 
 	@Test
 	void testFilterListOfMovil() {
-		fail("Not yet implemented");
+		List<Movil> listaMoviles = MovilOM.createMovil();
+		//Comprobando con una id que existe
+		listaMoviles = new FilterByTec(1L, null).filter(listaMoviles);
+		assertEquals(listaMoviles.size(), 1);
+		//Comprobando con una id que no existe
+		listaMoviles = new FilterByTec(15L, null).filter(listaMoviles);
+		assertEquals(listaMoviles.size(), 0);
 	}
 
 }
