e59b21077eb02f8dc8bf75a8076073fa7c85a32e
RadCres <raulduranc@hotmail.com>
2024-01-30 20:05:14 +0100

    Creacion ultimos tests
    Test creados:
    FilterByRam & FilterbyTec

---
+++ MovilesProject/src/test/java/com/moviles/filter/FilterByTecTest.java
@@ -0,0 +1,14 @@
+package com.moviles.filter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FilterByTecTest {
+
+	@Test
+	void testFilterListOfMovil() {
+		fail("Not yet implemented");
+	}
+
+}
