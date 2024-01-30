f21440f5f3d62a169ca13b149bca2b47d093db17
jesuscieshna <jchaveroc01@iesarroyoharnina.es>
2024-01-30 18:35:48 +0100

    cerrando issue#26

--- MovilesProject/src/main/java/com/moviles/mapper/MapDTOFilterToDecoratorFilter.java
+++ MovilesProject/src/main/java/com/moviles/mapper/MapDTOFilterToDecoratorFilter.java
@@ -1,12 +1,16 @@
 package com.moviles.mapper;
 import java.util.List;
 
+import com.moviles.filter.BaseDecoratorFilter;
 import com.moviles.filter.DecoratorFilter;
 import com.moviles.model.DTO.DTOMovilFilter;
 import com.moviles.model.entity.Movil;
 import com.moviles.util.Intervalo;
 import com.moviles.filter.FilterByPrecio;
+import com.moviles.filter.FilterByRam;
+import com.moviles.filter.FilterByTec;
 import com.moviles.filter.FilterByMarca;
+import com.moviles.filter.FilterByNfc;
 
 public class MapDTOFilterToDecoratorFilter implements Mapper<DTOMovilFilter, DecoratorFilter> {
 
@@ -14,7 +18,17 @@ public class MapDTOFilterToDecoratorFilter implements Mapper<DTOMovilFilter, Dec
 	public DecoratorFilter map(DTOMovilFilter t) {
 		DecoratorFilter<List<Movil>, Long> concrete = new FilterByMarca(t.getIdMarca());
 		Intervalo<Double> priceInterval = new Intervalo<Double>(t.getPrecioMin(), t.getPrecioMax());
-		FilterByPrecio filter = new FilterByPrecio(concrete, priceInterval);
+		DecoratorFilter filter = new FilterByPrecio(concrete, priceInterval);
+		if(!(t.getRamMax()==0 || t.getRamMax()==0)) {
+			Intervalo<Long> ramInterval = new Intervalo<Long>(Long.valueOf(t.getRamMin()), Long.valueOf(t.getRamMax()));
+			filter = new FilterByRam(ramInterval, filter);
+		}
+		if(t.getNfc()!=null) {
+			filter = new FilterByNfc(t.getNfc(), filter);
+		}
+		if(t.getIdTecnologiaPantalla()!=null) {
+			filter = new FilterByTec(t.getIdTecnologiaPantalla(), filter);
+		}
 		return filter;
 	}
 
