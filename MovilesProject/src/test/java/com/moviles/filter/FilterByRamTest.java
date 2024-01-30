dd27f1332629bd7348c2382f4aa31f8c2f5d6aa8
RadCres <raulduranc@hotmail.com>
2024-01-30 20:19:52 +0100

    FilterByRam hecho

--- MovilesProject/src/test/java/com/moviles/filter/FilterByRamTest.java
+++ MovilesProject/src/test/java/com/moviles/filter/FilterByRamTest.java
@@ -2,13 +2,24 @@ package com.moviles.filter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.List;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.moviles.OMs.MovilOM;
+import com.moviles.model.entity.Movil;
+import com.moviles.util.Intervalo;
+
+@SpringBootTest
 class FilterByRamTest {
 
 	@Test
 	void testFilterListOfMovil() {
-		fail("Not yet implemented");
+		List<Movil> listaMoviles = MovilOM.createMovil();
+		Intervalo<Long> intervalo = new Intervalo<Long>(7L, 11L);
+		listaMoviles = new FilterByRam(intervalo, null).filter(listaMoviles);
+		assertEquals(listaMoviles.size(), 1);
 	}
 
 }
