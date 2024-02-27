64526354371ed4a21ff3110a4b45948f618c6e4b
RadCres <raulduranc@hotmail.com>
2024-02-27 09:27:45 +0100

    Finished

--- MovilesProject/src/main/java/com/moviles/model/DTO/DTOAnuncio.java
+++ MovilesProject/src/main/java/com/moviles/model/DTO/DTOAnuncio.java
@@ -11,18 +11,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class DTOAnuncio {
 
-	private Long claveAnuncio;
 	private Long FKUsuario;
 	private Long referencia;
 	private String tipoMovilVenta;
 	private String estado;
 
-	public DTOAnuncio(Long fKUsuario, Long referencia, String tipoMovilVenta, String estado) {
-		super();
-		FKUsuario = fKUsuario;
-		this.referencia = referencia;
-		this.tipoMovilVenta = tipoMovilVenta;
-		this.estado = estado;
-	}
 
 }
--- MovilesProject/src/main/java/com/moviles/model/DTO/DTOUsuario.java
+++ MovilesProject/src/main/java/com/moviles/model/DTO/DTOUsuario.java
@@ -5,6 +5,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class DTOUsuario {
 
@@ -19,14 +26,6 @@ public class DTOUsuario {
 	@NotBlank(message = "no puede estar en blanco")
 	@Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d;,\\.]{8,}$", message = "Debe tener al menos 8 caracteres, contener al menos una mayúscula, una minúscula, un número y un símbolo entre coma, punto y coma, o punto")
 	private String password;
+	private String tipoUsuario;
 
-	// @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
-	// private Set<RoleEntity> roles;
-
-	public DTOUsuario(String email, String username, String password) {
-		super();
-		this.email = email;
-		this.username = username;
-		this.password = password;
-	}
 }
