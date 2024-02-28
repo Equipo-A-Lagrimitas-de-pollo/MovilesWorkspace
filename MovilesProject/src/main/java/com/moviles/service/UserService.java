ce3575739b4e369f0718e556e92133bbac857a85
pabloieshna <117895895+pabloieshna@users.noreply.github.com>
2024-02-28 16:13:43 +0100

    UserService Implementation

--- MovilesProject/src/main/java/com/moviles/service/UserService.java
+++ MovilesProject/src/main/java/com/moviles/service/UserService.java
@@ -1,11 +1,13 @@
 package com.moviles.service;
 
-import com.moviles.model.DTO.DTOUsuario;
+import com.moviles.model.DTO.DTOCreateUser;
+import com.moviles.model.entity.Usuario;
 
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface UserService {
-	public boolean createUser(DTOUsuario dto);
+	public boolean createUser(DTOCreateUser dto);
 	public boolean delete(String username);
-	public void refreshingToken(HttpServletResponse response, String authorizationHeader);
+	public void refreshingToken(HttpServletResponse response, String authorizationHeader) throws Exception;
+	public Usuario getUserByUsername(String username);
 }
--- MovilesProject/src/main/java/com/moviles/service/impl/UserDetailsServiceImpl.java
+++ MovilesProject/src/main/java/com/moviles/service/impl/UserDetailsServiceImpl.java
@@ -4,15 +4,22 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.moviles.repositories.UserRepository;
+
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-	//TODO Constructor
-	//TODO UserRepository
+private final UserRepository userRepository;
+	
+	public UserDetailsServiceImpl(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
 		return null;
+		//TODO
+		//Hace falta hacer el getRoles de usuario
 	}
 
 }
