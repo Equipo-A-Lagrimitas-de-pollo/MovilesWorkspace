61f2493312a895a70d5eb0c67c7850becab2fcb1
juanfranrb22 <120564690+juanfranrb22@users.noreply.github.com>
2024-02-28 13:58:19 +0100

    repos user y roles

---
+++ MovilesProject/src/main/java/com/moviles/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+//package com.moviles.repositories;
+//
+//import java.util.Optional;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.moviles.model.entity.ERole;
+//import com.moviles.model.entity.UserEntity;
+//
+//public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+//
+//	public Optional<RoleEntity> findByName(ERole erole);
+//}
