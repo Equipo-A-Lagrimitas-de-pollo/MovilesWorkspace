a9c0512692b05287b2561629168f09677793005b
jesuscieshna <jchaveroc01@iesarroyoharnina.es>
2024-02-29 19:05:18 +0100

    correccion repositories

--- MovilesProject/src/main/java/com/moviles/repositories/RoleRepository.java
+++ MovilesProject/src/main/java/com/moviles/repositories/RoleRepository.java
@@ -3,12 +3,14 @@ package com.moviles.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.moviles.model.entity.ERole;
 import com.moviles.model.entity.RoleEntity;
 
-
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+@Repository
+public interface RoleRepository extends MongoRepository<RoleEntity, Long> {
 
 	public Optional<RoleEntity> findByName(ERole erole);
 }
