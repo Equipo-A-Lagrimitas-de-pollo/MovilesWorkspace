f8d430117ee818ec2b80733496a7920204631663
jesuscieshna <jchaveroc01@iesarroyoharnina.es>
2024-03-02 20:06:22 +0100

    init bug

--- MovilesProject/src/main/java/com/moviles/model/entity/RoleEntity.java
+++ MovilesProject/src/main/java/com/moviles/model/entity/RoleEntity.java
@@ -1,31 +1,21 @@
 
 package com.moviles.model.entity;
 
-import org.bson.types.ObjectId;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@Entity
 @Document("roles")
 public class RoleEntity {
 	@Id
 	@Field("_id")
-	private ObjectId id;
-	@Enumerated(EnumType.STRING)
-	@Column(unique = true)
+	private String id;
+
 	@Field("nombreRol")
 	private ERole name;
 
